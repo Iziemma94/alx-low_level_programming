8a4d68fb66c5fb943e2dc4153e6865d1fb40aa86
root <taricarol632@>
2022-09-09 12:04:21 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,10 +6,13 @@
 int main(void)
 {
 	char num, alp;
+
 	for (num = '0'; num <= '9'; num++)
 		putchar(num);
 	for (alp = 'a'; alp <= 'f'; alp++)
 		putchar(alp);
+
 	putchar('\n');
+
 	return (0);
 }
