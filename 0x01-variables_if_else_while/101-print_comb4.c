140d0c7f67bd7fe9c34d77185a69c964a785be6b
root <taricarol632@>
2022-09-09 21:27:27 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int one;
-
 	int two;
-
 	int three;
 
 	for (one = 48; one <= 57; one++)
@@ -18,24 +17,29 @@ int main(void)
 
 		{
 			for (three = (two + 1); three <= 57; three++)
+
 			{
 				putchar (one);
+
 				putchar (two);
+
 				putchar (three);
+
 				if (one != 55)
 
 				{
 					putchar (44);
-					putchar (32)
+					putchar (32);
+
+				}
+
 			}
-			}
-			}
-			}
+
+		}
+
+	}
 
 	putchar ('\n');
 
 	return (0);
 }
-	
-				
-				
